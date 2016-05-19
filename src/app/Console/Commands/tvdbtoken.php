bca9baa05a9a1af97b5c95ad6824f75addbf11fe
Terence K <ikzelf@protonmail.com>
2016-05-19 22:07:52 +0200

    Remove old command, add command to get tvdb JWT token

---
+++ src/app/Console/Commands/tvdbtoken.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App;
+use Cache;
+
+class tvdbtoken extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tvdb:token';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get TVDB token';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $client = App::make('tvdb');
+
+        var_dump(Cache::get('tvdb_token'));
+        $this->info(Cache::get('tvdb_token'));
+    }
+}
