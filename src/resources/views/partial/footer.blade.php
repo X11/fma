eda1939db525eb6d611800b8eb1988b14375626c
Terence K <ikzelf@protonmail.com>
2016-05-21 13:44:49 +0200

    Fix footer, remove old route

--- src/resources/views/partial/footer.blade.php
+++ src/resources/views/partial/footer.blade.php
@@ -8,19 +8,18 @@
                         <li><a href="{{ url('/') }}">Home</a></li>
                         <li><a href="{{ url('/serie') }}">Series</a></li>
                         <li><a href="{{ url('/calender') }}">Calender</a></li>
-                        @if (Auth::user())
+                        @if (Auth::check())
                         <li><a href="{{ url('/watchlist') }}">Watchlist</a></li>
                         @endif
                     </ul>
                 </div>
             </div>
-            @if (Auth::user())
+            @if (Auth::check())
                 <div class="column is-2">
                     <div class="menu">
                         <p class="menu-label">Account</p>
                         <ul class="menu-list">
                             <li><a href="{{ url('/account/setting') }}">Settings</a></li>
-                            <li><a href="{{ url('/account/episodefile') }}">Files</a></li>
                             <li><a href="{{ url('/profile', [Auth::user()->id]) }}">Profile</a></li>
                         </ul>
                     </div>
