e02c96e90a046e86b8ac24bc2813f77618ab41f9
Terence K <ikzelf@protonmail.com>
2016-06-26 22:40:34 +0200

    Fix 404 title

--- src/resources/views/errors/404.blade.php
+++ src/resources/views/errors/404.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Be right back.</title>
+        <title>404 Page not found</title>
         <link href="https://fonts.googleapis.com/css?family=Lato:700" rel="stylesheet" type="text/css">
         <style>
             html, body { height: 100%; }
