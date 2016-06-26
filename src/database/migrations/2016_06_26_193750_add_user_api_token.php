6e429b601fd57564a8cc1fc24bee42a548017164
Terence K <ikzelf@protonmail.com>
2016-06-26 22:25:21 +0200

    str_random is not for uniqueness

--- src/database/migrations/2016_06_26_193750_add_user_api_token.php
+++ src/database/migrations/2016_06_26_193750_add_user_api_token.php
@@ -15,11 +15,21 @@ class AddUserApiToken extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->string('api_token', 71)->after('last_login')->unique();
         });
+        
+        function generateRandomString($length = 10) {
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $charactersLength = strlen($characters);
+            $randomString = '';
+            for ($i = 0; $i < $length; $i++) {
+                $randomString .= $characters[rand(0, $charactersLength - 1)];
+            }
+            return $randomString;
+        }
 
         $users = DB::table('users')->select('id')->get();
 
         foreach ($users as $user) {
-            DB::table('users')->where('id', $user->id)->update(['api_token' => str_random(70)]);
+            DB::table('users')->where('id', $user->id)->update(['api_token' => generateRandomString(70)]);
         }
         
     }
